bc4d49aad74265c30acbe810505ad54a75a8c2f5
Dave Pacia <dap971@g.harvard.edu>
2019-07-21 15:32:02 -0400

    Feature: support for both classifiers plotted together.

--- abalone_dpacia_sklearn/cli.py
+++ abalone_dpacia_sklearn/cli.py
@@ -2,6 +2,8 @@
 
 """Console script for abalone_dpacia_sklearn."""
 import sys
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 from .abalone_dpacia_sklearn import Abalone
 
 
@@ -9,7 +11,14 @@ def main(args=None):
     """Console script for abalone_dpacia_sklearn."""
     aba = Abalone()
     abalone_dataset = aba.load()
-    aba.train(abalone_dataset)
+    xtrain, xtest, ytrain, ytest = aba.split(abalone_dataset)
+
+    plt.figure()
+
+    plt.plot0 = aba.decisiontreeregressor(xtrain,xtest,ytrain,ytest)
+    plt.plot1 = aba.svr(xtrain,ytrain,xtest, ytest)
+
+    plt.show()
 
 
 if __name__ == "__main__":
